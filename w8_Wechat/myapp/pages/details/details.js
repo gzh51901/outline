746a125a8ccebb1b540091e936a8c171c2edd43c
aaron <jeroo@qq.com>
2019-06-05 16:06:36 +0800

    day8-3 百度音乐 详情播放页面

--- w8_Wechat/myapp/pages/details/details.js
+++ w8_Wechat/myapp/pages/details/details.js
@@ -58,7 +58,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: async function ({id:songid}) {
-    songid = 614044521;
+    // songid = 614044521;
 
     let data = await api.getData({
       method:'baidu.ting.song.play',
