cc54d8b8ffe7449d7e0ea8da88acc36be9034126
aaron <jeroo@qq.com>
2019-06-05 16:03:26 +0800

    day8-3 百度音乐项目-详情页与上线流程介绍

--- w8_Wechat/myapp/pages/details/details.js
+++ w8_Wechat/myapp/pages/details/details.js
@@ -1,18 +1,78 @@
 // pages/details/details.js
+let {api} = getApp();
+import regeneratorRuntime from '../../utils/runtime';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    myPlayer:null,
+    info:{},
+    paused:true
+  },
 
+  handleDownload(){
+    wx.downloadFile({
+      url: this.data.info.lrclink,
+      success(res){
+        console.log(res);
+
+        wx.showToast({
+          title: '下载成功',
+          icon: 'success',
+          duration: 3000
+        });
+      }
+    })
+  },
+
+  handlePlay(){console.log(this)
+    let { myPlayer,info,paused} = this.data;
+    if (!myPlayer){
+      // 创建播放器
+      myPlayer = wx.createInnerAudioContext();
+      myPlayer.src = info.file_link;
+      this.setData({
+        myPlayer
+      })
+    }
+
+    // 判断暂停与播放状态
+    // 决定是播放操作还是暂停操作
+    console.log('myPlayer.paused', myPlayer.paused)
+    if (paused){
+      myPlayer.play()
+    }else{
+      myPlayer.pause()
+    }
+    paused = !paused;
+
+    this.setData({
+      paused
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    console.log('details:',options)
+  onLoad: async function ({id:songid}) {
+    songid = 614044521;
+
+    let data = await api.getData({
+      method:'baidu.ting.song.play',
+      songid
+    });
+
+    console.log(data)
+
+    this.setData({
+      info:{
+        ...data.songinfo,
+        ...data.bitrate
+      }
+    })
   },
 
   /**
